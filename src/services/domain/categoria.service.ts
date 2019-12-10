ca7d89ed9c610cf3d4c0ecab15f637de50292d82
EmanuelFelipe <EmanuelFelipe0501@gmail.com>
2019-12-10 12:59:03 -0300

    adicionando categorias

---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core"
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class CategoriaService {
+
+  constructor(public http: HttpClient){
+
+  }
+
+  findAll(){
+
+  }
+
+}
