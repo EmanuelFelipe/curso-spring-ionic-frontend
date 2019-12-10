1647fbe6b23c16537a224bcdc704096e12f7d262
EmanuelFelipe <EmanuelFelipe0501@gmail.com>
2019-12-10 08:34:54 -0300

    Desabilitar menu da tela home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
 @IonicPage()
 
@@ -9,9 +9,17 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
+
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);//quando estiver na pagina home o menu sera desabilitado, puchar o menu do lado esquerdo
+    }
+
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);//quando sair do menu esse sera reabilitado
+    }
 
   login() {
     this.navCtrl.setRoot('CategoriasPage') //vai empilhar uma pagina em cima da outra(navegação da home para categorias)
